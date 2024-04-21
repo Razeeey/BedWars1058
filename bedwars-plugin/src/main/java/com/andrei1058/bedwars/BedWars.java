f5f025c16e4ceb24795dc527e8c03d7ba7dcb29c
leegod <lee20040919@gmail.com>
2024-04-21 12:25:31 +0800

    chore: Removed old tnt blast fix

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
@@ -283,10 +283,7 @@ public class BedWars extends JavaPlugin implements Listener {
                 new Inventory(), new Interact(), new RefreshGUI(), new HungerWeatherSpawn(), new CmdProcess(),
                 new FireballListener(), new EggBridge(), new SpectatorListeners(), new BaseListener(),
                 new TargetListener(), new LangListener(), new Warnings(this), new ChatAFK(),
-                new GameEndListener(), new DefaultStatsHandler(),
-
-                // new
-                new ExplosionListener()
+                new GameEndListener(), new DefaultStatsHandler()
         );
 
         if (config.getBoolean(ConfigPath.GENERAL_CONFIGURATION_HEAL_POOL_ENABLE)) {
